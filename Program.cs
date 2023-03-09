c08a3e90bb4d0b31e0ac7f73e1528db98505c17c
Vitor Santos <githubvitor@gmail.com>
2023-03-09 11:03:48 -0300

    create features add number with vector

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ void crescente(int nI, int nF)
     Console.WriteLine(nI);
     crescente(nI + 1, nF);
   }
+
 }
 void decrescente(int nI, int nF)
 {
@@ -24,7 +25,7 @@ void impares(int nI, int nF)
   {
     if (nI % 2 != 0)
     {
-      Console.WriteLine(nI);
+      Console.WriteLine("Caio no debug" + nI);
       impares(nI + 1, nF);
     }
   }
@@ -34,12 +35,23 @@ int somatorio(int nI, int nF)
 {
   if (nI < nF)
   {
+    Console.WriteLine(nI);
     return somatorio(nI + 1, nF) + nI;
   }
   else
   {
     return nI;
   }
+}
+
+int vetSomatorio(int[] vet, int nI, int nF)
+{
+  if (nI < nF)
+  {
+    return vetSomatorio(vet, nI + 1, nF) + vet[nI];
+  }
+  else
+    return vet[nI];
 }
 
 
@@ -53,6 +65,29 @@ while (op != "3")
   Console.WriteLine("3 - Sair");
   Console.Write("Opção desejada ");
   op = Console.ReadLine();
+
+  if (op == "2")
+  {
+    int[] vet = new int[10];
+    int i = 0;
+    int num = 1;
+
+    while (num != 0)
+    {
+      i += 1;
+      Console.WriteLine("Digite um numero: ");
+      num = Convert.ToInt32(Console.ReadLine());
+      vet[i] = num;
+    }
+    int s;
+    s = vetSomatorio(vet, 1, i);
+    Console.WriteLine(s);
+  }
+
+
+
+
+
 
   if (op == "1")
   {
